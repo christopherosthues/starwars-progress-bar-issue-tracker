469acbcfc501f21529ee1a472e5b4e7e07379e5c
Christopher Osthues <christopher.osthues@gmail.com>
2025-02-21 20:29:36 +0100

    chore: Update EF core tools

--- server/src/StarWarsProgressBarIssueTracker.Infrastructure/Database/Migrations/IssueTrackerContextModelSnapshot.cs
+++ server/src/StarWarsProgressBarIssueTracker.Infrastructure/Database/Migrations/IssueTrackerContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace StarWarsProgressBarIssueTracker.Infrastructure.Database.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.0")
+                .HasAnnotation("ProductVersion", "9.0.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
